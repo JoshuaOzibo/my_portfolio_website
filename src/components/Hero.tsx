73a6b75c890d3b8d703bcb238d9a64ff44495bd1
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-05-15 10:50:32 +0100

    Enhance Hero and OverView components with improved layout and functionality
    - Updated the Hero component to include a resume image and buttons for downloading the resume, enhancing user interaction.
    - Refined the layout for better responsiveness and visual appeal, including adjustments to spacing and alignment.
    - Modified the OverView component to improve margin handling for better layout consistency across different screen sizes.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,26 +2,52 @@
 
 import { Particles } from "@/components/magicui/particles";
 import OverView from "@/components/OverView";
+import Image from "next/image";
+import ResumeImage from "@/assets/my-image.webp";
 
 import { WordRotate } from "@/components/magicui/word-rotate";
 
 export default function Hero() {
   return (
     <section className="">
-      <div className="relative overflow-hidden h-full md:h-[700px] pt-24 w-full">
-        <div className="absolute mx-10 inset-0 flex">
-          <div className="w-1/3 ">
+      <div className="relative lg:h-[700px] h-screen pt-24 w-full">
+        <div className="absolute lg:flex justify-between lg:mx-20 mx-6 space-y-10 lg:space-y-0 inset-0  items-center">
+          <div className="lg:w-1/2 lg:m-0 m-auto w-full">
             <h1 className="xl:text-6xl lg:text-5xl md:text-4xl text-3xl mt-10 font-bold text-start pt-20 text-white">
-              Turning <WordRotate className="" words={["Ideas", "Codes", "Logic"]} />into Real-World Projects That Make an Impact
+              Turning{" "}
+              <WordRotate className="text-[#86cad9]" words={["Ideas", "Codes", "Logic"]} />
+              into Real-World Projects That Make an Impact
             </h1>
             <p className="text-white text-sm mt-3 font-light">
               Hi, I’m Joshua a front-end developer passionate about crafting
               meaningful UIs that deliver results.
             </p>
             <p>Currently open to remote opportunities.</p>
+
+            <div className="lg:flex mt-5 lg:space-x-5 space-x-0 space-y-5 lg:space-y-0 lg:mx-0 w-full block gap-2">
+              <div className="m-auto w-full lg:m-0">
+                <button className="bg-white w-full  text-black px-4 py-2 rounded-md">
+                  Download Resume
+                </button>
+              </div>
+
+              <div className="m-auto w-full lg:m-0">
+                <button className="bg-white w-full text-black px-4 py-2 rounded-md">
+                  Download Resume
+                </button>
+              </div>
+            </div>
           </div>
+
+          <Image
+            src={ResumeImage}
+            className="rounded-full lg:mx-0 mx-auto"
+            width={350}
+            height={350}
+            alt="ResumeImage"
+          />
         </div>
-        <Particles />
+        <Particles className="h-full" />
       </div>
       <OverView />
     </section>
--- src/components/OverView.tsx
+++ src/components/OverView.tsx
@@ -19,7 +19,7 @@ const OverView = () => {
   }, []);
 
   return (
-    <section className="z-[1] py-10">
+    <section className="z-[1] py-10 lg:mt-0 mt-20">
        <div className="flex justify-center gap-5 items-center">
         {HeroCardData.slice(0, isMobile ? 1 : HeroCardData.length).map((card, index) => (
           <MagicCard card={{card}} className="h-[180px] mx-6 z-[5] w-[500px] bg-[#18181b] " index={index} key={index}>
